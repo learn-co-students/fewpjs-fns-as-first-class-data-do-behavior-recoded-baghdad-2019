ec8bf15678e17c29e5686867e8ed9face501707f
zozo <zahraa.jalaa99@gmail.com>
2019-09-15 19:43:59 +0300

    solved

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 /* Given Code, don't edit */
 
 function handleClick(e) {
-  const timeString = document.getElementById('time').value
-  displayMessage(greet(timeString))
+    const timeString = document.getElementById('time').value
+    displayMessage(greet(timeString))
 }
 
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(string) {
+    let time = parseInt(string.split(':').join(''));
+    if (time < 1200) {
+        return 'Good Morning';
+    } else if (time >= 1200 && time <= 1700) {
+        return 'Good Afternoon';
+    } else if (time > 1700) {
+        return 'Good Evening';
+    }
+
+}
 /* Write your implementation of displayMessage() */
+function displayMessage(string) {
+    document.getElementById('greeting').innerHTML = string;
+}
